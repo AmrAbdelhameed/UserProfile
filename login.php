dc5c9294310395b00e31f0d4d3676719df7fa9f4
Amr Abdelhameed <amrabdelhameedfcis123@gmail.com>
2017-02-25 00:49:24 +0200

    login edit design

--- login.php
+++ login.php
@@ -1,8 +1,6 @@
 <?php 
 
- $connection = new mysqli('localhost','root','','database_example') or die ("Database connection failed");
-
-//$sql = "insert into user (username,password) values ('mariam','ahmed13')";
+ $connection = new mysqli('localhost','root','','data_examples') or die ("Database connection failed");
 
 if (isset ($_POST['submit']))
 {
@@ -45,31 +43,90 @@ if (isset ($_POST['submit']))
 }
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <title>Login Page</title>
-    <link rel="stylesheet" type="text/css" href="css_file.css">
-    </head>
+  <title>OSC SimpleApp</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+
+    <link href="http://getbootstrap.com/assets/css/ie10-viewport-bug-workaround.css" rel="stylesheet">
+
+    <script src="http://getbootstrap.com/assets/js/ie-emulation-modes-warning.js"></script>
+
+    <link href="http://getbootstrap.com/examples/carousel/carousel.css" rel="stylesheet">
+    
+    <link rel="icon" href="osc_icon.png">
+</head>
 <body>
 
-<h2>Login Form</h2>
-
-    <form action="login.php" method="post">
-        <div class="container">
-            <label for="username"><b>Email</b></label>
-            <input type="text" placeholder="Enter Your Email" name="username" required>
-        
-              <label for="password"><b>Password</b></label>
-            <input type="password" placeholder="Enter Your Password" name="password" required>
-          
-            <input type="checkbox" checked="checked"> Remember me
-        </div>
-        <div class="container" style="background-color:#ffffff">
-    <button type="submit" name="submit" class="cancelbtn">Login</button>
-    <span class="psw">If You don't have account , please <a href="http://localhost/Examples/register.php" target="_self">Register</a></span>
+    <nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+      <a class="navbar-brand">OSC</a>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="http://localhost/Examples/navbar.php">Home</a></li>
+        <li class="dropdown">
+          <a class="dropdown-toggle" data-toggle="dropdown" href="#">Page 1 <span class="caret"></span></a>
+          <ul class="dropdown-menu">
+            <li><a href="#">Page 1-1</a></li>
+            <li><a href="#">Page 1-2</a></li>
+            <li><a href="#">Page 1-3</a></li>
+          </ul>
+        </li>
+        <li><a href="#">Page 2</a></li>
+        <li><a href="#">Page 3</a></li>
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="http://localhost/Examples/register.php"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
+        <li><a href="http://localhost/Examples/login.php"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+      </ul>
+    </div>
   </div>
-        </form>
+</nav>
+    
+<div class="container">
+  <h1 style="text-align: center;">Login</h1> 
+  <form class="form-horizontal" action="login.php" method="post">
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="email">Email :</label>
+      <div class="col-sm-10">
+        <input type="text" placeholder="Enter Your Email" name="username" required class="form-control">
+      </div>
+    </div>
+    <div class="form-group">
+      <label class="control-label col-sm-2" for="pwd">Password :</label>
+      <div class="col-sm-10">          
+        <input type="password" placeholder="Enter Your Password" name="password" required class="form-control">
+      </div>
+    </div>
+    <div class="form-group">        
+      <div class="col-sm-offset-2 col-sm-10">
+        <div class="checkbox">
+          <label><input type="checkbox"> Remember me</label>
+        </div>
+      </div>
+    </div>
+    <div class="form-group">        
+      <div class="col-sm-offset-2 col-sm-10">
+        <button type="submit" name="submit" class="btn btn-default">Login</button>
+          <span>If You don't have account , please <a href="http://localhost/Examples/register.php" target="_self">Register</a></span>
+      </div>
+    </div>
+  </form>
+</div>
 
 </body>
 </html>
